acc5f07cf5ff45d17f5117fb5a303f88edabac79
beatriz <beatrizdib@hotmail.com>
2017-09-28 12:42:31 -0300

    dd.py

--- dd.py
+++ dd.py
@@ -3,12 +3,26 @@ import numpy as np
 class DispersionDiscretization:
     def __init__(self):
         pass
-    def fit (self, X):
+    #index must be a list containing the index numbers from each column to be selected from the array, and X must be an array in the format of a list, in which each list within the main list must be a line of the array
+    def fit (self, X, index, y):
+        mean_list = []
+        sd_list = []
+        y_list =[]
         Xlinha = np.array(X)
-        print Xlinha
-        print 
-        print np.mean(Xlinha[:,0])
+        i=0
+        for e in index:
+            i=+1
+            m = np.mean(Xlinha[:,e])
+            sd = np.std(Xlinha[:,e])
+            w =(y[i])*sd
+            y_list.append(w)
+            mean_list.append (m)
+            sd_list.append (sd)
+        print mean_list
+        print sd_list
+        print y_list
+        #print np.mean(Xlinha[:,0])
 
 dd = DispersionDiscretization()
 X = [[1,2,3],[3,2,2],[3,2,2],[3,2,2]]
-dd.fit(X)
+dd.fit(X, [1, 2], [1,0.5])
