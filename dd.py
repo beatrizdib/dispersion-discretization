19ac1a7018baa160d776f146308988968bc53a38
beatriz <beatrizdib@hotmail.com>
2017-09-28 11:53:18 -0300

    ainda nao modifiquei nada

--- dd.py
+++ dd.py
@@ -1,1 +1,14 @@
-#escrevi aqui o meu código.
+import numpy as np
+
+class DispersionDiscretization:
+    def __init__(self):
+        pass
+    def fit (self, X):
+        Xlinha = np.array(X)
+        print Xlinha
+        print 
+        print np.mean(Xlinha[:,0])
+
+dd = DispersionDiscretization()
+X = [[1,2,3],[3,2,2],[3,2,2],[3,2,2]]
+dd.fit(X)
