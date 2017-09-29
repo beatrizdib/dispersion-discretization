c57fa7364b4e2a9bab9c9f8b1d54056919c7a486
beatriz <beatrizdib@hotmail.com>
2017-09-29 12:24:15 -0300

    fit

--- dd.py
+++ dd.py
@@ -5,24 +5,24 @@ class DispersionDiscretization:
         pass
     #index_list must be a list containing the index numbers from each column from the array to be modified
     #X must be an array typed in the format of a list, in which each list within the main list must be a line of the array
-    #y must be a number which the standard deviation from each column will be divided by
-    def fit (self, X, index_list, y):
+    #lmbda must be a number which the standard deviation from each column will be divided by
+    def fit (self, X, index_list, lmbda):
         mean_list = []
         sd_list = []
-        y_list =[]
+        lmbda_list =[]
         Xlinha = np.array(X)
         i=0
         for e in index:
             i=+1
             m = np.mean(Xlinha[:,e])
             sd = np.std(Xlinha[:,e])
-            w =sd/(y[i])
-            y_list.append(w)
+            w =sd/(lmbda[i])
+            lmbda_list.append(w)
             mean_list.append (m)
             sd_list.append (sd)
         print mean_list
         print sd_list
-        print y_list
+        print lmda_list
         #print np.mean(Xlinha[:,0])
 
 dd = DispersionDiscretization()
