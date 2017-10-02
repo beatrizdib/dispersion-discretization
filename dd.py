35c4f31d089cfadeb04d48e8f56e7ca53c7270cd
beatriz <beatrizdib@hotmail.com>
2017-10-02 16:57:14 -0300

    erros inacabados

--- dd.py
+++ dd.py
@@ -7,6 +7,18 @@ class DispersionDiscretization:
     #X must be an array typed in the format of a list, in which each list within the main list must be a line of the array
     #lmbda must be a number which the standard deviation from each column will be divided by
     def fit (self, X, index_list, lmbda):
+        for i in xrange(len(X)):
+            for j in xrange(len(X[i])):
+                if not (type(X[i][j]) is float or type(X[i][j]) is int):
+                    raise(ValueError("All elements in array X must be integers or floats"))
+        for i in xrange(len(X)):
+            if len(X[i]) != len(X[0]):
+                raise(IndexError("All lines in the array must have the same length"))
+        for e in index_list:
+            if e>len(X):
+                raise(IndexError("Index "+str(e)+" is out of range for array"+str(X)))
+
+
         mean_list = []
         sd_list = []
         lmbda_list =[]
@@ -22,8 +34,19 @@ class DispersionDiscretization:
             sd_list.append (sd)
         print mean_list
         print sd_list
-        #print np.mean(Xlinha[:,0])
+
+
 
 dd = DispersionDiscretization()
 X = [[1,2,3],[3,2,2],[3,2,2],[3,2,2]]
-dd.fit(X, [1, 2], [1,0.5])
+
+try:
+
+    dd.fit(X, [1, 2,10], [1,0.5])
+except IndexError as ie:
+    print ie
+except ValueError as ve:
+    print ve 
+
+
+
